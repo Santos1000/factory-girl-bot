2b42b246d1bce37c239e2bf5e06c4d8bff80bdf2
barbara santos <barbaransantos1@gmail.com>
2022-06-23 11:13:49 -0300

    read url from body

--- spec/httparty/httparty_spec.rb
+++ spec/httparty/httparty_spec.rb
@@ -1,11 +1,10 @@
 describe 'httparty' do
-  it 'content-type', vcr: { cassette_name: 'jsonstub' } do
+  it 'content-type', vcr: { cassette_name: 'jsonstub', match_requests_on: [:body] } do
     # stub_request(:get, "https://jsonplaceholder.typicode.com/posts/2").
     #   to_return(status: 200, body: "", headers: {'content-type': 'application/json'})
-    response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/2')
+    response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/4')
     content_type = response.headers['content-type']
     # puts content_type
     expect(content_type).to match(/application\/json/)
-
   end
 end
